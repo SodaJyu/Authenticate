df2761008b670f5267ea6d966014928a001f5864
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 12:30:51 +0900

    feat: add functions to authModel

--- src/models/authModel.ts
+++ src/models/authModel.ts
@@ -2,3 +2,42 @@ export {};
 
 let knex = require("../knex");
 
+const usersTable = "users";
+
+const getAllUsers = () => {
+    return knex.select({
+        id: "id",
+        username: "username",
+        password: "password"
+    })
+    .from(usersTable);
+};
+
+const getUserById = (id: Number) => {
+    return knex.select({
+        id: "id",
+        username: "username",
+        password: "password"
+    })
+    .from(usersTable)
+    .where({id: id})
+    .first();
+};
+
+const createUser = (user: Object) => {
+    return knex.insert(user).into("users").catch(console.error);
+};
+
+const updateUser = (id: Number, updatedUser: Object) => {
+    return knex("users")
+    .update(updatedUser)
+    .where({id: id})
+    .catch(console.error());
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser
+}
