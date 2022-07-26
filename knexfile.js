0275a890d7d93fce3cf6d928269a636b398fcc29
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 13:40:24 +0900

    update: add more env variables to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,8 @@ require("dotenv").config({
   });
 
   module.exports = {
-    PORT: process.env.DB_PORT
+    PORT: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   }
