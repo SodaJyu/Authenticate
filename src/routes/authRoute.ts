d2ee99069c82711b532a9becd5c7b2725f84392f
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 14:10:33 +0900

    feat: add auth route

---
+++ src/routes/authRoute.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const authController = require("../controllers/authController");
+
+const router = express.Router();
+
+router.get("/", authController.getAllUsers);
+router.get("/:id", authController.getUserById);
+router.post("/", authController.createUser);
+router.put("/:id", authController.updateUser);
+
+module.exports = router;
