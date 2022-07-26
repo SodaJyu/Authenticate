24cfd415856e65ce59fd53790390c85812f42ac1
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 15:28:44 +0900

    bug: put route issue

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/", authController.getAllUsers);
 router.get("/:id", authController.getUserById);
 router.post("/", authController.createUser);
-router.put("/:id", authController.updateUser);
+//router.put("/:id", authController.updateUser);
 
 module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 const PORT = 8080 || process.env.PORT;
+const authRoutes = require('./routes/authRoute');
 
 app.use(express.json());
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`ARRR App be listenin' on this here ${PORT}`);
