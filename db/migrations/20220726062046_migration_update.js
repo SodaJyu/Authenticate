5e69fdaddbfd49f9e36cc63dac4570bf345be70d
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 15:26:42 +0900

    feat: add salt column to users table

---
+++ db/migrations/20220726062046_migration_update.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.alterTable("users", (table) => {
+    table.string("salt").notNullable().unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTable("users");
+};
