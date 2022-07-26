7de060d9365cc690f4136b7443e9e0ae67a5caaf
Dominic Lowes <chaiskeng@gmail.com>
2022-07-26 15:29:29 +0900

    chore: update knexfile variables

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,14 @@ require("dotenv").config({
   });
 
   module.exports = {
+    client: "pg",
+    connection: process.env.DATABASE_URL || {
     PORT: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
   }
