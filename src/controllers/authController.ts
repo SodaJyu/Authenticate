0b0f6b51ebd182d84fa479c16430d22560fede67
Dominic Lowes <chaiskeng@gmail.com>
2022-09-14 16:08:18 +0900

    feat: implement password hashing function and comment out salt

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,8 @@
 const authModel = require("../models/authModel");
 import { Request, Response } from 'express';
-//const createSalt = require('../auth/salt');
 import * as crypto from 'crypto';
+import * as argon2 from 'argon2';
+import { config } from 'dotenv';
 
 module.exports = {
     async getAllUsers(req: Request, res: Response) {
@@ -10,24 +11,42 @@ module.exports = {
     },
 
     async getUserById(req: Request, res: Response){
-        const user = await authModel.getUserById();
+        const user: Object = await authModel.getUserById();
         res.send(user);
     },
 
     async createUser(req: Request, res: Response){
         let { id, username, password } = req.body;
-        let currentSalt: String;
-        const createSalt = () => {
-            const salt = crypto.randomBytes(32).toString('base64');
-            currentSalt = salt;
-            return salt;
+        //let currentSalt;
+
+        const hashConfig = {
+            parallelism: 2,
+            memoryCost: 64000,
+            timeCost: 3,
+        }
+
+        // const generateSalt = () => {
+        //     const salt = crypto.randomBytes(32).toString('base64');
+        //     console.log(salt);
+        //     currentSalt = salt;
+        //     return salt;
+        // }
+
+        const hashPassword = async (password, /*salt*/) => {
+            return await argon2.hash(password, {
+                ...hashConfig,
+                //salt,
+            });
+            
         };
+        //const salt = generateSalt()
+        const hashedPassword = await hashPassword(password, /*salt*/);
 
         const userObj = {
             id: id,
             username: username,
-            password: password += createSalt(),
-            salt: currentSalt,
+            password: hashedPassword,
+           // salt: currentSalt,
         };
 
         if (id) {
@@ -36,6 +55,7 @@ module.exports = {
             await authModel.createUser(userObj);
 
         }
+        //currentSalt = '';
         res.status(200).send(userObj);
     },
 
